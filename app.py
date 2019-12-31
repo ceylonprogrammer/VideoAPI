b945d849ead3f34e41be6f1cec135b4ed4d232e3
sameera <sameerad177@gmail.com>
2019-12-31 11:35:46 +0530

    add imports and create mongodb connection

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template, stream_with_context
+from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify
+from flask import request, Response
+
+from email.mime.multipart import MIMEMultipart
+
+from datetime import datetime, timedelta
+from simpleflake import simpleflake
+from pymongo import MongoClient, DESCENDING
+
+import os
+import socket
+import json
+import smtplib
+import jinja2
+import subprocess
+import base64
+
+# Create Database connection with pymongo
+print(socket.gethostbyaddr(socket.gethostname()))
+
+# DB Connection
+
+db_client = MongoClient('localhost', 27017)
+# print(db_client.server_info()) get connection Details
+print("Mongodb Connection Successful")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
